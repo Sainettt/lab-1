18b56dbedb858d8a2dfe1407ae9069964f51bc52
Danyil Fiut <danyilfiut@gmail.com>
2025-03-25 17:40:30 +0100

    routing.js

--- routing/routing.js
+++ routing/routing.js
@@ -1,16 +1,33 @@
 // 📦 Zaimportuj moduł odpowiedzialne za routing poszczególnych części aplikacji.
 // 📦 Zaimportuj obiekt STATUS_CODE.
+const homeRouting = require('./home.js')
+const productRouting = require('./product.js')
+const logoutRouting = require('./logout.js')
+const { STATUS_CODE } = require('../constants/statusCode.js')
 
 // 🏗 Stwórz tutaj funkcję 'requestRouting', która będzie obsługiwać zapytania HTTP.
-// Podpowiedź: const requestRouting = (request, response) => {
-// 🏗 Tutaj stwórz logowanie do konsoli informacji, mówiące o typie logowania (INFO), dacie, metodzie oraz url żądania.
-// 🏗 Tutaj stwórz podstawowy 'request routing' dla ścieżek '/', zawierającej /product' oraz '/logout'. Przekaż `request` i `routing` do odpowiednio routingu.
+const requestRouting = (request, response) => {
+  const { url, method } = request
+  const date = new Date().toISOString()
 
-// 🏗 Obsłuż specjalny przypadek, jeśli użytkownik zostanie przekierowany na ścieżkę /kill, aplikacja się zamknie.
-// 🏗 Stwórz również logowanie do konsoli informacji, mówiące o typie logowania (PROCESS), dacie oraz informację, że wylogowowyanie zostało wywołane a aplikacja zamknie się.
+  console.log(`INFO [${date}]: ${method} – ${url}`)
 
-// 🏗 Tutaj stwórz obsługę przypadku, jeśli żądany URL nie istnieje. Zwróć wtedy błąd 404.
-// 🏗 Stwórz również logowanie do konsoli informacji, mówiące o typie logowania (ERROR), dacie oraz informację, że żądany url nie istnieje.
-//  };
+  if (url === '/') {
+    return homeRouting(request, response)
+  } else if (url.startsWith('/product')) {
+    return productRouting(request, response)
+  } else if (url === '/logout') {
+    return logoutRouting(request, response)
+  } else if (url === '/kill') {
+    console.log(
+      `PROCESS [${date}]: logout has been initiated and the application will be closed`
+    )
+    process.exit()
+  } else {
+    console.log(`ERROR [${date}]: requested url ${url} doesn’t exist.`)
+    response.writeHead(STATUS_CODE.NOT_FOUND, { 'Content-Type': 'text/html' })
+    response.end('<h1>404 Not Found</h1>')
+  }
+}
 
-// 🔧 Wyeksportuj funkcję 'requestRouting', aby inne moduł mogły jej używać.
+module.exports = { requestRouting }
